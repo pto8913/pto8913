2bda082e560d1ee3f59ed353061b5a498bbca023
pto8913 <nn3kskyou@gmail.com>
2023-04-22 22:28:23 +0900

    update

--- assets/js/create_table_of_contents.js
+++ assets/js/create_table_of_contents.js
@@ -1,60 +1,59 @@
-function CreateTableOfContents(window, document) {
-  const selector = document.querySelector('.post-body');
-  if (!selector) {
-    return
-  }
+// <!-- 目次を作成する先 -->
+// <div id="toc"></div>
 
-  const toc = document.createElement('details');
-  const sum = document.createElement('summary');
-  const list = document.createElement('ul');
-  toc.id = 'toc';
-  toc.open = true;
-  sum.className = 'toc-title';
-  sum.textContent = '目次';
-  list.className = 'toc-container';
-  toc.appendChild(sum);
-  toc.appendChild(list);
+document.addEventListener('DOMContentLoaded', function () {
+    var contentsList = document.getElementById('toc'); // 目次を追加する先(table of contents)
+    var div = document.createElement('div'); // 作成する目次のコンテナ要素
 
-  const headings = selector.querySelectorAll('h2, h3, h4');
-  if (headings.length == 0) {
-    return
-  }
-  headings[0].parentNode.insertBefore(toc, headings[0]);
-  const order = [];
-  const stack = [{level: 1, element: list}];
+    // .entry-content配下のh2、h3要素を全て取得する
+    var matches = document.querySelectorAll('.entry-content h2, .entry-content h3');
 
-  // 事前処理
-  headings.forEach((heading) => {
-    const level = parseInt(heading.tagName.substring(1))
-    order.push(level);
-  });
+    // 取得した見出しタグ要素の数だけ以下の操作を繰り返す
+    matches.forEach(function (value, i) {
+        // 見出しタグ要素のidを取得し空の場合は内容をidにする
+        var id = value.id;
+        if(id === '') {
+            id = value.textContent;
+            value.id = id;
+        }
 
-  headings.forEach((heading, i) => {
-    const level = parseInt(heading.tagName.substring(1));
-    const next = order[i + 1];
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const id = 'toc-' + (i + 1);
-    const ul = document.createElement('ul');
+        // 要素がh2タグの場合
+        if(value.tagName === 'H2') {
+            var ul = document.createElement('ul');
+            var li = document.createElement('li');
+            var a = document.createElement('a');
 
-    // 目次要素の生成
-    a.textContent = heading.textContent;
-    a.href = `#${id}`;
-    li.appendChild(a);
-    if (level < next) {
-      li.appendChild(ul);
-    }
+            // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
+            a.innerHTML = value.textContent;
+            a.href = '#' + value.id;
+            li.appendChild(a)
+            ul.appendChild(li);
 
-    // リンク先の生成
-    heading.id = id;
+            // コンテナ要素である<div>の中に要素を追加する
+            div.appendChild(ul);
+        }
 
-    // 階層構造の生成
-    let parent;
-    do {
-      parent = stack.pop();
-    } while (parent.level >= level);
-    parent.element.appendChild(li);
-    stack.push(parent);
-    stack.push({level: level, element: ul});
-  });
-}(window, document);
+        // 要素がh3タグの場合
+        if(value.tagName === 'H3') {
+            var ul = document.createElement('ul');
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+
+            // コンテナ要素である<div>の中から最後の<li>を取得する。
+            var lastUl = div.lastElementChild;
+            var lastLi = lastUl.lastElementChild;
+
+            // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
+            a.innerHTML = value.textContent;
+            a.href = '#' + value.id;
+            li.appendChild(a)
+            ul.appendChild(li);
+
+            // 最後の<li>の中に要素を追加する
+            lastLi.appendChild(ul);
+        }
+    });
+
+    // 最後に画面にレンダリング
+    contentsList.appendChild(div);
+});
