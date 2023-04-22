258c317c7915d782aa4d270e7d447a1e37466fbb
pto8913 <nn3kskyou@gmail.com>
2023-04-22 17:47:31 +0900

    update

--- assets/js/create_table_of_contents.js
+++ assets/js/create_table_of_contents.js
@@ -6,7 +6,7 @@
     This software is released under the MIT License.
     http://opensource.org/licenses/mit-license.php
 */
-function CreateTableOfContents(){
+// 設定
 const TOC_INSERT_SELECTOR = '#toc';              // [セレクター指定] 目次を挿入する要素 querySelector用
 const HEADING_SELECTOR    = 'h1,h2,h3,h4,h5,h6'; // [セレクター指定] 収集する見出し要素 querySelectorAll用
 const LINK_CLASS_NAME     = 'tocLink';           // [クラス名] 目次用aタグに追加するクラス名     .無し
@@ -16,7 +16,6 @@ const headingElements     = document.querySelectorAll(HEADING_SELECTOR);
 const layer = [];
 let id = 0;
 const uid   = () =>`${ID_NAME}${id++}`;
-let links = null;
 let oldRank = -1;
 try {
     const createLink = (el) => {
@@ -39,19 +38,6 @@ try {
     const appendToc = (el, toc) => {
         el.appendChild(toc.cloneNode(true));
     };
-    const tocHighlight = (e) => {
-        const sy = window.pageYOffset;
-        const ey = sy + document.documentElement.clientHeight;
-        let tocHighlightEl = null;
-        links.forEach( (el) => {
-            const targetEl = document.querySelector(el.hash);
-            const y = sy + targetEl.getBoundingClientRect().top ;
-            el.classList.remove("active") ;
-            if(sy < y &&  y < ey)tocHighlightEl = el;
-            if(sy > y) tocHighlightEl = el;
-        });
-        if(tocHighlightEl)tocHighlightEl.classList.add("active");
-    };
     headingElements.forEach( (el) => {
         let rank   = Number(el.tagName.substring(1));
         let parent = findParentElement(layer, rank, -1);
@@ -64,22 +50,6 @@ try {
         oldRank = rank;
     });
     if (layer.length) appendToc(tocInsertElement, findParentElement(layer, 0, 1));
-    links = document.querySelectorAll(`.${LINK_CLASS_NAME}`);
-    links.forEach((el) => {
-        el.addEventListener("click",(e)=>{
-            const targetEl = document.querySelector(el.hash);
-            scrollTo(0, window.pageYOffset + targetEl.getBoundingClientRect().top - 100);
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    });
-    tocHighlight();
-    window.addEventListener("scroll", tocHighlight);
-    console.log("create_table_of_contents.js is success");
-
 } catch (e) {
     //error 
-    console.log("create_table_of_contents.js is failled");
-    // document.write(e);
-}
 }
