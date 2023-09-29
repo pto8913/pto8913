71713d1b3bce34b2357f52eda76432292682a039
pto8913 <nn3kskyou@gmail.com>
2023-09-29 09:59:01 +0900

    add

---
+++ assets/js/Collapsible.js
@@ -0,0 +1,20 @@
+var coll = document.getElementsByClassName("Collapsible");
+
+for (var collIdx = 0; coll.length; ++collIdx)
+{
+    coll[collIdx].addEventListener(
+        "click",
+        function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block")
+            {
+                content.style.display = "none";
+            }
+            else
+            {
+                content.style.display = "block";
+            }
+        }
+    );
+}
