e645736a46b173e2bd3239e52b476676d4853fb2
pto8913 <nn3kskyou@gmail.com>
2023-10-02 09:56:40 +0900

    a

--- assets/js/hatenablog.js
+++ assets/js/hatenablog.js
@@ -1,4 +1,4 @@
-{/* <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> */}
+{/* <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 import jQuery from "https://code.jquery.com/jquery-3.4.1.min.js";
 
@@ -82,4 +82,4 @@ function getArchive(urlInfo){
         }
     ).responseText;
     return data;
-}
+} */}
