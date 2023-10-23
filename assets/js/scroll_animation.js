14ee651b08111483bbd6cc794f2e4286b29bbf1c
pto8913 <nn3kskyou@gmail.com>
2023-10-23 09:50:17 +0900

    a

--- assets/js/scroll_animation.js
+++ assets/js/scroll_animation.js
@@ -1,26 +1,10 @@
-fetch('https://code.jquery.com/jquery-3.6.3.min.js').then(r=>r.text()).then(t=>eval(t)).then(
-  ()=>{
-    if($('#fade-in').length == 0)
-    {
-      console.log( "no tag end--->" + $('#fade-in').length ) ;
-      return ;
-    }
-    Init();
-  }
-);
-
-function Init()
-{
-  
-}
-
 var windowHeight = $(window).height();
 $("#wh span").text(windowHeight);
 $(window).on(
   "scroll", 
   function()
   {
-    var scroll_top = $(window).scroll_top();
+    var scroll_top = $(window).scrollTop();
     $("#scroll span").text(scroll_top);
 
     $(".fade_box").each(
