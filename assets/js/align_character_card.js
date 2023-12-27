03b2fcf55906458546e050ffd22de431d7933dad
pto8913 <nn3kskyou@gmail.com>
2023-12-27 17:35:17 +0900

    fix chara card image, fix align card

--- assets/js/align_character_card.js
+++ assets/js/align_character_card.js
@@ -2,7 +2,7 @@
 window.onload = function()
 {
     var character_card_description_div = document.getElementById('character_card_description');
-    var character_card_image_div = document.getElementById('character_card_image');
+    var character_card_image_div = document.getElementsByClassName('Hoshimori_character_card_img');
 
     if (character_card_image_div.clientHeight < character_card_description_div.clientHeight)
     {
