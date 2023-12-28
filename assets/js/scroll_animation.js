60d2e35c5570d30b917c692444dbbadfca3dc9a4
pto8913 <nn3kskyou@gmail.com>
2023-12-28 21:35:57 +0900

    a

--- assets/js/scroll_animation.js
+++ assets/js/scroll_animation.js
@@ -1,9 +1,9 @@
-var viewportHeight = $(window).height();
-$("#wh span").text(viewportHeight);
 $(window).on(
   "scroll", 
   function()
   {
+    var viewportHeight = $(window).height();
+    $("#wh span").text(viewportHeight);
     var documentPosition = $(window).scrollTop();
     $("#scroll span").text(documentPosition);
 
@@ -16,7 +16,7 @@ $(window).on(
 
         var visibleArea = documentPosition + viewportHeight;
         var elemArea = elemOffset + elemHeight;
-
+        console.log(documentPosition, viewportHeight, elemOffset, elemHeight);
         if (documentPosition <= elemOffset && visibleArea >= elemArea)
         {
           $(this).addClass("fadein");
