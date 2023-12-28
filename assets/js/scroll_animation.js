40a9c93a48b2d189b6e978d703b8401e5678375a
pto8913 <nn3kskyou@gmail.com>
2023-12-28 22:10:05 +0900

    a

--- assets/js/scroll_animation.js
+++ assets/js/scroll_animation.js
@@ -14,6 +14,7 @@ $(window).on(
         var elem_pos = $(this).offset().top;
         $(this).find(".box_posspan").text(Math.floor(elem_pos));
 
+        console.log(windowHeight, scroll_top, elem_pos);
         if (scroll_top >= elem_pos - windowHeight)
         {
           $(this).addClass("fadein");
