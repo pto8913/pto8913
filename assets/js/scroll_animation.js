27b74d427f2a5775d4d714e04f354725a43e21e0
pto8913 <nn3kskyou@gmail.com>
2023-12-29 10:01:24 +0900

    update scroll animation

--- assets/js/scroll_animation.js
+++ assets/js/scroll_animation.js
@@ -1,21 +1,51 @@
 $(window).on(
+  "load",
+  function()
+  {
+    var windowHeight = $(window).height();
+    $("#wh span").text(windowHeight);
+
+    $(".fade_box").each(
+      function()
+      {
+        var elemTop = $(this).offset().top;
+        $(this).find(".box_posspan").text(Math.floor(elemTop));
+
+        if (0 <= elemTop && elemTop <= windowHeight)
+        {
+          $(this).addClass("fadein");
+          $(this).removeClass("fadeout");
+        }
+        else
+        {
+          $(this).removeClass("fadein");
+          $(this).addClass("fadeout");
+        }
+      }
+    )
+  }
+)
+
+$(window).on(
   "scroll", 
   function()
   {
     var windowHeight = $(window).height();
     $("#wh span").text(windowHeight);
 
-    var scroll_top = $(window).scrollTop();
-    $("#scroll span").text(scroll_top);
+    var viewportTop = $(window).scrollTop();
+    $("#scroll span").text(viewportTop);
 
     $(".fade_box").each(
       function()
       {
-        var elem_pos = $(this).offset().top;
-        $(this).find(".box_posspan").text(Math.floor(elem_pos));
+        var elemTop = $(this).offset().top;
+        $(this).find(".box_posspan").text(Math.floor(elemTop));
 
-        console.log(windowHeight, scroll_top, elem_pos);
-        if (scroll_top >= elem_pos - windowHeight)
+        var viewportHeight = viewportTop + windowHeight;
+        console.log(windowHeight, viewportTop, elemTop);
+
+        if (viewportTop <= elemTop && elemTop <= viewportHeight)
         {
           $(this).addClass("fadein");
           $(this).removeClass("fadeout");
