4ce313a589bc31aece8bcdeb2f2e57766bedd589
pto8913 <nn3kskyou@gmail.com>
2023-12-29 14:29:35 +0900

    update character card, add scroll animation

--- assets/js/scroll_animation.js
+++ assets/js/scroll_animation.js
@@ -1,30 +1,32 @@
-$(window).on(
-  "load",
-  function()
-  {
-    var windowHeight = $(window).height();
-    $("#wh span").text(windowHeight);
+$(window).on("load", initialize())
+$(window).on("resize", initialize())
+function initialize()
+{
+  var windowHeight = $(window).height();
+  $("#wh span").text(windowHeight);
 
-    $(".fade_box").each(
-      function()
+  $("#scroll span").text(viewportTop);
+
+  $(".fade_box").each(
+    function()
+    {
+      var elemTop = $(this).offset().top;
+      var elemBottom = $(this).offset().bottom;
+      $(this).find(".box_posspan").text(Math.floor(elemTop));
+
+      if (0 <= elemTop && elemTop <= windowHeight || 0 <= elemBottom && elemBottom <= windowHeight)
       {
-        var elemTop = $(this).offset().top;
-        $(this).find(".box_posspan").text(Math.floor(elemTop));
-
-        if (0 <= elemTop && elemTop <= windowHeight)
-        {
-          $(this).addClass("fadein");
-          $(this).removeClass("fadeout");
-        }
-        else
-        {
-          $(this).removeClass("fadein");
-          $(this).addClass("fadeout");
-        }
+        $(this).addClass("fadein");
+        $(this).removeClass("fadeout");
       }
-    )
-  }
-)
+      else
+      {
+        $(this).removeClass("fadein");
+        $(this).addClass("fadeout");
+      }
+    }
+  )
+}
 
 $(window).on(
   "scroll", 
@@ -40,12 +42,13 @@ $(window).on(
       function()
       {
         var elemTop = $(this).offset().top;
+        var elemBottom = $(this).offset().bottom;
         $(this).find(".box_posspan").text(Math.floor(elemTop));
 
         var viewportHeight = viewportTop + windowHeight;
-        console.log(windowHeight, viewportTop, elemTop);
+        // console.log(windowHeight, viewportTop, elemTop);
 
-        if (viewportTop <= elemTop && elemTop <= viewportHeight)
+        if (viewportTop <= elemTop && elemTop <= viewportHeight || viewportTop <= elemBottom && elemBottom <= viewportHeight)
         {
           $(this).addClass("fadein");
           $(this).removeClass("fadeout");
