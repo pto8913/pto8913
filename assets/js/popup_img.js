0d951932438f00b5e5c5a85ad52b242ac9a02fcf
pto8913 <nn3kskyou@gmail.com>
2024-04-20 07:30:05 +0900

    as

--- assets/js/popup_img.js
+++ assets/js/popup_img.js
@@ -1,16 +1,17 @@
-const popup_container = document.getElementById('popup_img_container');
-const popup_img = popup_container.querySelector('img');
-
-popup_container.addEventListener(
-    'click', 
-    () => popup_container.style.display = 'none'
-);
 
 document.querySelectorAll('popup_img').forEach(
     i => {
         i.addEventListener(
             'click',
             () => {
+                const popup_container = document.getElementById('popup_img_container');
+                const popup_img = popup_container.querySelector('img');
+
+                popup_container.addEventListener(
+                    'click', 
+                    () => popup_container.style.display = 'none'
+                );
+
                 popup_img.src = i.src;
                 popup_container.style.display = 'block';
             }
